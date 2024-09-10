6a7339ccb7659f125677f2798460f4e56d4d3217
Oliver Butler <dev@oliverbutler.uk>
2024-09-10 14:59:07 +0100

    feat: wip adding blog

--- pages/index.go
+++ pages/index.go
@@ -26,16 +26,15 @@ func Index(ctx context.Context) g.Node {
 	for _, post := range posts {
 		blogTiles = append(blogTiles, Div(Class("bg-neutral-950 p-4 rounded-md"),
 			Img(Src(post.HeroImage), Class("w-full rounded-md")),
-			H2(Class("text-2xl font-bold"), g.Text(post.Title)),
-			P(Class("text-gray-200"), g.Text(post.PubDate.FormattedString())),
+			H3(Class("text-2xl font-bold"), A(Href("/post/"+post.Slug), g.Text(post.Title))),
+			Sub(Class("text-gray-200"), g.Text(post.PubDate.FormattedString())),
 			P(Class("text-gray-100"), g.Text(post.Description)),
-			A(Href("/post/"+post.Slug), g.Text("Read more")),
 		))
 	}
 
 	return components.Page(Div(
 		components.NavBar("/"),
-		Div(Class("max-w-4xl mx-auto grid grid-cols-2 gap-4"),
+		Div(Class("max-w-4xl mx-auto grid grid-cols-4 gap-4"),
 			g.Group(blogTiles),
 		),
 	))
