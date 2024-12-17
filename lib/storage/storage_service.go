05ef8efd35f3de1e0980a84c7122d7a1f0bc5126
Oliver Butler <dev@oliverbutler.uk>
2024-12-17 21:53:59 +0000

    feat: migrate to local storage

--- lib/storage/storage_service.go
+++ lib/storage/storage_service.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"io"
 	"oliverbutler/lib/environment"
+	"os"
 	"path/filepath"
-
-	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 )
+
+const LocalStorageRoot = "/home/olly/Desktop/omo"
 
 // FileItem represents a file in the storage system
 type FileItem struct {
@@ -29,53 +29,38 @@ type StorageRepo interface {
 	GetItemContent(ctx context.Context, item *FileItem) (io.ReadCloser, error)
 }
 
-// MinioStorageRepo implements StorageRepo for MinIO
-type MinioStorageRepo struct {
-	client *minio.Client
-}
+// LocalStorageRepo implements StorageRepo for local file system
+type LocalStorageRepo struct{}
 
 type StorageService struct {
 	StorageRepo StorageRepo
 }
 
 func NewStorageService(env *environment.EnvironmentService) (*StorageService, error) {
-	storageRepo, err := NewMinioStorageRepo(env.GetMinioEndpoint(), env.GetMinioAccessKey(), env.GetMinioSecretKey(), env.GetMinioUseSSL())
-	if err != nil {
-		return nil, err
-	}
-
+	storageRepo := NewLocalStorageRepo()
 	return &StorageService{StorageRepo: storageRepo}, nil
 }
 
-// NewMinioStorageRepo creates a new MinioStorageRepo
-func NewMinioStorageRepo(endpoint, accessKeyID, secretAccessKey string, useSSL bool) (*MinioStorageRepo, error) {
-	client, err := minio.New(endpoint, &minio.Options{
-		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-		Secure: useSSL,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return &MinioStorageRepo{client: client}, nil
+// NewLocalStorageRepo creates a new LocalStorageRepo
+func NewLocalStorageRepo() *LocalStorageRepo {
+	return &LocalStorageRepo{}
 }
 
 // PutItem uploads an item to the specified bucket and folder
-func (m *MinioStorageRepo) PutItem(ctx context.Context, bucket, folder string, name string, reader io.Reader, size int64, contentType string) (*FileItem, error) {
-	objectName := filepath.Join(folder, name)
-
-	// Ensure bucket exists
-	err := m.client.MakeBucket(ctx, bucket, minio.MakeBucketOptions{})
+func (l *LocalStorageRepo) PutItem(ctx context.Context, bucket, folder string, name string, reader io.Reader, size int64, contentType string) (*FileItem, error) {
+	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	err := os.MkdirAll(filepath.Dir(fullPath), 0755)
 	if err != nil {
-		// Check if the bucket already exists
-		exists, errBucketExists := m.client.BucketExists(ctx, bucket)
-		if errBucketExists == nil && exists {
-			// Bucket already exists, continue
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 
-	_, err = m.client.PutObject(ctx, bucket, objectName, reader, size, minio.PutObjectOptions{ContentType: contentType})
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, reader)
 	if err != nil {
 		return nil, err
 	}
@@ -90,75 +75,63 @@ func (m *MinioStorageRepo) PutItem(ctx context.Context, bucket, folder string, n
 }
 
 // GetItem retrieves metadata for an item
-func (m *MinioStorageRepo) GetItem(ctx context.Context, bucket, folder, name string) (*FileItem, error) {
-	objectName := filepath.Join(folder, name)
-	info, err := m.client.StatObject(ctx, bucket, objectName, minio.StatObjectOptions{})
+func (l *LocalStorageRepo) GetItem(ctx context.Context, bucket, folder, name string) (*FileItem, error) {
+	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	info, err := os.Stat(fullPath)
 	if err != nil {
 		return nil, err
 	}
 
 	return &FileItem{
 		Name:        name,
-		Size:        info.Size,
-		ContentType: info.ContentType,
+		Size:        info.Size(),
+		ContentType: "", // Local storage doesn't store content type
 		Folder:      folder,
 		Bucket:      bucket,
 	}, nil
 }
 
 // DeleteItem removes an item from storage
-func (m *MinioStorageRepo) DeleteItem(ctx context.Context, bucket, folder, name string) error {
-	objectName := filepath.Join(folder, name)
-	return m.client.RemoveObject(ctx, bucket, objectName, minio.RemoveObjectOptions{})
+func (l *LocalStorageRepo) DeleteItem(ctx context.Context, bucket, folder, name string) error {
+	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	return os.Remove(fullPath)
 }
 
 // ListItems lists all items in a folder
-func (m *MinioStorageRepo) ListItems(ctx context.Context, bucket, folder string) ([]*FileItem, error) {
+func (l *LocalStorageRepo) ListItems(ctx context.Context, bucket, folder string) ([]*FileItem, error) {
 	var items []*FileItem
+	fullPath := filepath.Join(LocalStorageRoot, bucket, folder)
 
-	objectCh := m.client.ListObjects(ctx, bucket, minio.ListObjectsOptions{
-		Prefix:    folder,
-		Recursive: true,
-	})
-
-	for object := range objectCh {
-		if object.Err != nil {
-			return nil, object.Err
+	err := filepath.Walk(fullPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-
-		items = append(items, &FileItem{
-			Name:        filepath.Base(object.Key),
-			Size:        object.Size,
-			ContentType: object.ContentType,
-			Folder:      filepath.Dir(object.Key),
-			Bucket:      bucket,
-		})
+		if !info.IsDir() {
+			relPath, _ := filepath.Rel(fullPath, path)
+			items = append(items, &FileItem{
+				Name:        info.Name(),
+				Size:        info.Size(),
+				ContentType: "", // Local storage doesn't store content type
+				Folder:      filepath.Dir(relPath),
+				Bucket:      bucket,
+			})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	return items, nil
 }
 
 // GetItemContent retrieves the content of an item
-func (m *MinioStorageRepo) GetItemContent(ctx context.Context, item *FileItem) (io.ReadCloser, error) {
-	objectName := filepath.Join(item.Folder, item.Name)
-	return m.client.GetObject(ctx, item.Bucket, objectName, minio.GetObjectOptions{})
+func (l *LocalStorageRepo) GetItemContent(ctx context.Context, item *FileItem) (io.ReadCloser, error) {
+	fullPath := filepath.Join(LocalStorageRoot, item.Bucket, item.Folder, item.Name)
+	return os.Open(fullPath)
 }
 
-func (m *MinioStorageRepo) DeleteFolder(ctx context.Context, bucket, folder string) error {
-	objectCh := m.client.ListObjects(ctx, bucket, minio.ListObjectsOptions{
-		Prefix:    folder,
-		Recursive: true,
-	})
-
-	for object := range objectCh {
-		if object.Err != nil {
-			return object.Err
-		}
-
-		if err := m.client.RemoveObject(ctx, bucket, object.Key, minio.RemoveObjectOptions{}); err != nil {
-			return err
-		}
-	}
-
-	return nil
+func (l *LocalStorageRepo) DeleteFolder(ctx context.Context, bucket, folder string) error {
+	fullPath := filepath.Join(LocalStorageRoot, bucket, folder)
+	return os.RemoveAll(fullPath)
 }
