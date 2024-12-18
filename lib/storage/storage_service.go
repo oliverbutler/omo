1d6de82794c56cdb68290d7562f1c7b5655c0285
Oliver Butler <dev@oliverbutler.uk>
2024-12-18 20:02:19 +0000

    feat: add root storage path

--- lib/storage/storage_service.go
+++ lib/storage/storage_service.go
@@ -8,8 +8,6 @@ import (
 	"path/filepath"
 )
 
-const LocalStorageRoot = "/home/olly/Desktop/omo"
-
 // FileItem represents a file in the storage system
 type FileItem struct {
 	Name        string
@@ -30,25 +28,29 @@ type StorageRepo interface {
 }
 
 // LocalStorageRepo implements StorageRepo for local file system
-type LocalStorageRepo struct{}
+type LocalStorageRepo struct {
+	env *environment.EnvironmentService
+}
 
 type StorageService struct {
 	StorageRepo StorageRepo
 }
 
 func NewStorageService(env *environment.EnvironmentService) (*StorageService, error) {
-	storageRepo := NewLocalStorageRepo()
+	storageRepo := NewLocalStorageRepo(env)
 	return &StorageService{StorageRepo: storageRepo}, nil
 }
 
 // NewLocalStorageRepo creates a new LocalStorageRepo
-func NewLocalStorageRepo() *LocalStorageRepo {
-	return &LocalStorageRepo{}
+func NewLocalStorageRepo(env *environment.EnvironmentService) *LocalStorageRepo {
+	return &LocalStorageRepo{env}
 }
 
 // PutItem uploads an item to the specified bucket and folder
 func (l *LocalStorageRepo) PutItem(ctx context.Context, bucket, folder string, name string, reader io.Reader, size int64, contentType string) (*FileItem, error) {
-	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	rootStoragePath := l.env.GetRootStoragePath()
+
+	fullPath := filepath.Join(rootStoragePath, bucket, folder, name)
 	err := os.MkdirAll(filepath.Dir(fullPath), 0755)
 	if err != nil {
 		return nil, err
@@ -76,7 +78,9 @@ func (l *LocalStorageRepo) PutItem(ctx context.Context, bucket, folder string, n
 
 // GetItem retrieves metadata for an item
 func (l *LocalStorageRepo) GetItem(ctx context.Context, bucket, folder, name string) (*FileItem, error) {
-	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	rootStoragePath := l.env.GetRootStoragePath()
+
+	fullPath := filepath.Join(rootStoragePath, bucket, folder, name)
 	info, err := os.Stat(fullPath)
 	if err != nil {
 		return nil, err
@@ -93,14 +97,18 @@ func (l *LocalStorageRepo) GetItem(ctx context.Context, bucket, folder, name str
 
 // DeleteItem removes an item from storage
 func (l *LocalStorageRepo) DeleteItem(ctx context.Context, bucket, folder, name string) error {
-	fullPath := filepath.Join(LocalStorageRoot, bucket, folder, name)
+	rootStoragePath := l.env.GetRootStoragePath()
+
+	fullPath := filepath.Join(rootStoragePath, bucket, folder, name)
 	return os.Remove(fullPath)
 }
 
 // ListItems lists all items in a folder
 func (l *LocalStorageRepo) ListItems(ctx context.Context, bucket, folder string) ([]*FileItem, error) {
+	rootStoragePath := l.env.GetRootStoragePath()
+
 	var items []*FileItem
-	fullPath := filepath.Join(LocalStorageRoot, bucket, folder)
+	fullPath := filepath.Join(rootStoragePath, bucket, folder)
 
 	err := filepath.Walk(fullPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -127,11 +135,15 @@ func (l *LocalStorageRepo) ListItems(ctx context.Context, bucket, folder string)
 
 // GetItemContent retrieves the content of an item
 func (l *LocalStorageRepo) GetItemContent(ctx context.Context, item *FileItem) (io.ReadCloser, error) {
-	fullPath := filepath.Join(LocalStorageRoot, item.Bucket, item.Folder, item.Name)
+	rootStoragePath := l.env.GetRootStoragePath()
+
+	fullPath := filepath.Join(rootStoragePath, item.Bucket, item.Folder, item.Name)
 	return os.Open(fullPath)
 }
 
 func (l *LocalStorageRepo) DeleteFolder(ctx context.Context, bucket, folder string) error {
-	fullPath := filepath.Join(LocalStorageRoot, bucket, folder)
+	rootStoragePath := l.env.GetRootStoragePath()
+
+	fullPath := filepath.Join(rootStoragePath, bucket, folder)
 	return os.RemoveAll(fullPath)
 }
